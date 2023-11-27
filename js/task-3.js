935ee1add440ee5bcecf9cd26e11c850857c4abc
Vladislav Zarovniy <vladislavzarovnii13@gmail.com>
2023-11-27 02:11:50 +0200

    add-hw-2

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,24 @@
 'use strict';
 
-function getElementWidth(content, padding, border) {
-  
-  return parseFloat(content) + (parseFloat(padding) * 2) + (parseFloat(border) * 2);
+function checkForSpam(message) {
 
+  let str = message.toUpperCase();
+
+  if (str.includes('SPAM')) {
+    return true;
+  } else if (str.includes('SALE')) {
+    return true;
+  }
+  else {
+    return false;
+  }
+ 
 };
 
-console.log(getElementWidth("50px", "8px", "4px"));
-
-console.log(getElementWidth("60px", "12px", "8.5px"));
-
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
