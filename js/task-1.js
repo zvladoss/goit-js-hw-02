1e09d84a6f117293f8651e5b65589d21e23b647e
Vladislav Zarovniy <vladislavzarovnii13@gmail.com>
2023-11-27 10:56:44 +0200

    fixed-task-1-5-log

--- js/task-1.js
+++ js/task-1.js
@@ -5,7 +5,7 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
 
   let totalPrice = quantity * pricePerDroid;
 
-  if (totalPrice < customerCredits) {
+  if (totalPrice <= customerCredits) {
     return  `You ordered ${quantity} droids worth ${totalPrice} credits!`
   } else {
     return  `Insufficient funds!`
